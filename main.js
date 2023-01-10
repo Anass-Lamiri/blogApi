8192c1e330dad3fc488d706d89902579d773ce8d
Anas Lamiri <anaslamiri23@gmail.com>
2023-01-10 06:56:36 +0100

    fix: showing data according to the inputed data and some code improvement

--- main.js
+++ main.js
@@ -3,34 +3,55 @@ import { api } from './api.js'
 
 
 async function createPosts() {
-	let posts = await api();
 	let range = document.getElementById('range')
-	let submit = document.getElementById('submit')
-
-	posts.length = range.value
+	let con = document.querySelector('.con')
+	let range_text = document.querySelector('.range_text')
 	
-	posts.forEach((post, index) => {
-		let mainDiv = document.createElement('div')
-		mainDiv.className = 'blog_post'
+	onload = async () => {
+		let posts = await api();
+		range.value = range.min
+		range_text.innerHTML = `you're seeing now ${range.value} posts`
+		posts.length = range.value
+		posts.forEach((post) => {
+			createUi(post)
+		});
+	}
+	range.onchange = async () => {
+		let posts = await api();
 		
-		let h2 = document.createElement('h2')
-		let h2Text = document.createTextNode(post.title)
-		h2.className = 'title'
-		h2.appendChild(h2Text)
-		
-		let p = document.createElement('p')
-		let pText = document.createTextNode(post.body)
-		p.className = 'text'
-		p.appendChild(pText)
-	
-		mainDiv.appendChild(h2)
-		mainDiv.appendChild(p)
-	
-		document.getElementById('app').appendChild(mainDiv)
-	});
+		range_text.innerHTML = `you're seeing now ${range.value} posts`
 
+		// removing the old data in the Ui before showing the new data 
+		con.innerHTML = ""
+
+		posts.length = range.value
+		posts.forEach((post) => {
+			createUi(post)
+		});
+	}
 }
 
 createPosts() 
+
+
+function createUi(post) {
+	let mainDiv = document.createElement('div')
+	mainDiv.className = 'blog_post'
+	
+	let h2 = document.createElement('h2')
+	let h2Text = document.createTextNode(post.title)
+	h2.className = 'title'
+	h2.appendChild(h2Text)
+	
+	let p = document.createElement('p')
+	let pText = document.createTextNode(post.body)
+	p.className = 'text'
+	p.appendChild(pText)
+
+	mainDiv.appendChild(h2)
+	mainDiv.appendChild(p)
+
+	document.querySelector(".con").appendChild(mainDiv)
+}
 
 
